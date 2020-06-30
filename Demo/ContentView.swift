6c02a049f6148eac9f9de478e2c9f7f86656021c
Cherry <cherry@Cherrys-MacBook-Pro.local>
2020-06-30 10:37:00 +0530

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Cherry on 30/06/20.
+//  Copyright © 2020 Cherry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
